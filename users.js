ebc15ad36342a561d3d972394b94a10b54d2060a
realsan444 <121910515+realsan444@users.noreply.github.com>
2025-06-05 07:33:35 +1000

    Create users.js

---
+++ users.js
@@ -0,0 +1,116 @@
+const usersData = [
+  {
+    user: "nashst",
+    password: "Darthvadercat",
+    display: "Nasharee",
+    rank: "owner"
+  },
+  {
+    user: "aidan",
+    password: "aidst",
+    display: "Aidan",
+    rank: "user"
+  },
+  {
+    user: "hailey",
+    password: "haist",
+    display: "Hailey",
+    rank: "user"
+  },
+  {
+    user: "hudson",
+    password: "hudst",
+    display: "Hudson",
+    rank: "user"
+  },
+  {
+    user: "augusto",
+    password: "augst",
+    display: "Augusto",
+    rank: "user"
+  },
+  {
+    user: "luke",
+    password: "lukst",
+    display: "Luke",
+    rank: "user"
+  },
+  {
+    user: "mollyk",
+    password: "molst",
+    display: "MollyK",
+    rank: "user"
+  },
+  {
+    user: "soren",
+    password: "sorst",
+    display: "Soren",
+    rank: "user"
+  },
+  {
+    user: "liam",
+    password: "liast",
+    display: "Liam",
+    rank: "user"
+  },
+  {
+    user: "sacha",
+    password: "sacst",
+    display: "Sacha",
+    rank: "user"
+  },
+  {
+    user: "asher",
+    password: "ashst",
+    display: "Asher",
+    rank: "moneymaker"
+  },
+    {
+    user: "sannyboy",
+    password: "Sanny57001",
+    display: "San",
+    rank: "vip"
+  },
+  {
+    user: "robsan",
+    password: "robst",
+    display: "Robsan",
+    rank: "user"
+  },
+  {
+    user: "georgie",
+    password: "geost",
+    display: "Georgie",
+    rank: "user"
+  },
+  {
+    user: "alice",
+    password: "alist",
+    display: "Alice",
+    rank: "user"
+  },
+  {
+    user: "skylah",
+    password: "skyst",
+    display: "Skylah",
+    rank: "user"
+  },
+  {
+    user: "leo",
+    password: "leost",
+    display: "Leo",
+    rank: "user"
+  },
+  {
+    user: "jacob",
+    password: "jacst",
+    display: "Jacob",
+    rank: "user"
+  },
+  {
+    user: "isacc",
+    password: "isast",
+    display: "Isacc",
+    rank: "user"
+  }
+];
